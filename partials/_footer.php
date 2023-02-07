15173fe5a8128312938e7d2f95e3457fb6cc2437
unknown <vannkhryssfrancisco@gmail.com>
2023-02-07 20:02:50 +0800

    first commit

---
+++ partials/_footer.php
@@ -0,0 +1,3 @@
+<div class="footer container-fluid bg-dark text-light">
+    <p class="text-center py-2 mb-0">© 2022 THE COMPUTER HAVEN, Inc. All rights reserved. <a href="" target="_blank" rel="noopener noreferrer">@vkfrancisco</a></p>
+</div>
